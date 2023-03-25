c7fef651b2c1f0c7188a372e44225b6a6b4ab710
nhyatt <nhyatt@smoothnet.org>
2023-03-24 23:40:10 -0500

    k8s needs tls 1.2

--- cmd/webhook/httpServer.go
+++ cmd/webhook/httpServer.go
@@ -56,7 +56,7 @@ func httpServer() {
 		WriteTimeout: time.Duration(cfg.WebServerWriteTimeout) * time.Second,
 		IdleTimeout:  time.Duration(cfg.WebServerIdleTimeout) * time.Second,
 		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS13,
+			MinVersion: tls.VersionTLS12,
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
